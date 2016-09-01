6d471ed383e30401a818593ca9ffccdf4735f23b
ganyi <ganyiwelldone@gmail.com>
2016-09-02 00:23:13 +0800

    test case

--- src/com/hashmap/test/HashMapDIYTest.java
+++ src/com/hashmap/test/HashMapDIYTest.java
@@ -2,93 +2,121 @@ package com.hashmap.test;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.hashmap.HashMapDIY;
 
 public class HashMapDIYTest {
 
-	@Test
-	public void testHashMapDIY() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testHashMapDIYInt() {
-		fail("Not yet implemented");
-	}
+	HashMapDIY<Integer, String> hashMap = new HashMapDIY<>();
 
 	@Test
 	public void testSize() {
-		fail("Not yet implemented");
+		hashMap.put(1, "a");
+		hashMap.put(2, "b");
+		hashMap.put(3, "c");
+		hashMap.put(4, "d");
+		hashMap.put(5, "e");
+		hashMap.put(6, "f");
+		assertEquals(6,hashMap.size());
 	}
 
 	@Test
 	public void testIsEmpty() {
-		fail("Not yet implemented");
+		assertEquals(true,hashMap.isEmpty());
+		hashMap.put(1, "a");
+		hashMap.put(2, "b");
+		hashMap.put(3, "c");
+		hashMap.put(4, "d");
+		hashMap.put(5, "e");
+		hashMap.put(6, "f");
+		assertEquals(false,hashMap.isEmpty());
 	}
 
 	@Test
 	public void testContainsKey() {
-		fail("Not yet implemented");
+		hashMap.put(1, "a");
+		hashMap.put(2, "b");
+		hashMap.put(3, "c");
+		hashMap.put(4, "d");
+		hashMap.put(5, "e");
+		hashMap.put(6, "f");
+		assertEquals(false,hashMap.containsKey(8));
+		assertEquals(true,hashMap.containsKey(6));
 	}
 
 	@Test
 	public void testContainsValue() {
-		fail("Not yet implemented");
+		hashMap.put(1, "a");
+		hashMap.put(2, "b");
+		hashMap.put(3, "c");
+		hashMap.put(4, "d");
+		hashMap.put(5, "e");
+		hashMap.put(6, "f");
+		assertEquals(true, hashMap.containsValue("a"));
+		assertEquals(false, hashMap.containsValue("aa"));
 	}
 
 	@Test
 	public void testGet() {
-		fail("Not yet implemented");
+		hashMap.put(1, "a");
+		hashMap.put(2, "b");
+		hashMap.put(3, "c");
+		hashMap.put(4, "d");
+		hashMap.put(5, "e");
+		hashMap.put(6, "f");
+		assertEquals("e",hashMap.get(5));
 	}
 
 	@Test
 	public void testPut() {
-		fail("Not yet implemented");
+		hashMap.put(6, "f");
+		assertEquals(true, hashMap.containsKey(6));
+		assertEquals(true, hashMap.containsValue("f"));
 	}
 
 	@Test
 	public void testRemove() {
-		fail("Not yet implemented");
+		hashMap.put(1, "a");
+		hashMap.put(2, "b");
+		hashMap.put(3, "c");
+		hashMap.put(4, "d");
+		hashMap.put(5, "e");
+		hashMap.put(6, "f");
+		hashMap.remove(2);
+		assertEquals(false, hashMap.containsKey(2));
+		assertEquals(false, hashMap.containsValue("b"));
 	}
 
 	@Test
 	public void testPutAll() {
-		fail("Not yet implemented");
+		Map<Integer,String> map = new HashMap<>();
+		map.put(1, "asd");
+		map.put(2, "sds");
+		map.put(3, "dvs");
+		map.put(4, "gsa");
+		map.put(5, "oup");
+		hashMap.putAll(map);
+		assertEquals(5,hashMap.size());
+		assertEquals(true,hashMap.containsKey(1));
+		assertEquals(true,hashMap.containsValue("asd"));
 	}
 
 	@Test
 	public void testClear() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testKeySet() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testValues() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testEntrySet() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testHash() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testIndex() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testToString() {
-		fail("Not yet implemented");
+		hashMap.put(1, "a");
+		hashMap.put(2, "b");
+		hashMap.put(3, "c");
+		hashMap.put(4, "d");
+		hashMap.put(5, "e");
+		hashMap.put(6, "f");
+		hashMap.clear();
+		assertEquals(true, hashMap.isEmpty());
 	}
 
 }
