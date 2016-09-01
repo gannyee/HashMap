36eed9f654b97bec691a0a6554987290c40e5719
ganyi <ganyiwelldone@gmail.com>
2016-09-02 00:23:53 +0800

    update

--- src/com/hashmap/test/HashMapDIYTest.java
+++ src/com/hashmap/test/HashMapDIYTest.java
@@ -5,8 +5,6 @@ import static org.junit.Assert.*;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.hashmap.HashMapDIY;
